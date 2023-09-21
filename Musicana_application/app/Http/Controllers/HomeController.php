71584124b4c4ab051722a247d8e6d2661f815571
shaileshprajapati1 <shaileshprajapati966@gmail.com>
2023-09-21 13:48:23 +0530

    data

---
+++ Musicana_application/app/Http/Controllers/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+       $user = Auth::user();
+    //    dd($user);
+       if($user['role_id'] == 1){
+        return redirect("admin/admindashboard");
+       } 
+        return view('home');
+      
+    //    dd($user->role_id);
+    }
+}
--- Musicana_application/routes/web.php
+++ Musicana_application/routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome1');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::prefix('admin')->group(function(){
+    Route::view('/admindashboard',"admin.admindashboard");
+
 });
